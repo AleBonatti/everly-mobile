cc45dc45a9ff002c35ffae333682debe09d65951
AleBonatti <la.seppia@gmail.com>
2026-08-25 12:45:58 +0200

    Bump version to 1.2.0

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
     expo: {
         name: "Everly",
         slug: "everly-mobile",
-        version: "1.1.0",
+        version: "1.2.0",
         orientation: "portrait",
         icon: "./assets/icon.png",
         userInterfaceStyle: "light",
